13c5185e100a72098fe57215e6b32012246f3def
steve <sskone@gmail.com>
2011-06-28 16:13:51 +0100

    Created JPhotoFrame test class

---
+++ test/fi/iki/jka/JPhotoFrameTest.java
@@ -0,0 +1,38 @@
+package fi.iki.jka;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dev
+ * Date: 28/06/2011
+ * Time: 15:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class JPhotoFrameTest {
+
+    private JPhotoFrame jphotoFrame;
+
+    @Before
+    public void instantiateJPhotoFrame() throws Exception
+    {
+        setJphotoFrame(new JPhotoFrame("Test Frame Name",new String[] {"/resources/pics/directory.png"}));
+    }
+
+
+    @Test
+    public void testActionPerformed() throws Exception {
+
+        getJphotoFrame().actionPerformed(null);
+
+    }
+
+    public JPhotoFrame getJphotoFrame() {
+        return jphotoFrame;
+    }
+
+    public void setJphotoFrame(JPhotoFrame jphotoFrame) {
+        this.jphotoFrame = jphotoFrame;
+    }
+}
